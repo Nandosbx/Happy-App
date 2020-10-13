8f9b562530ec7383499c9897944db8501d0d6165
Fernando Batista <nandosbx@gmail.com>
2020-10-12 23:56:11 -0300

    Initial Commit

---
+++ web/src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+//SECTION Styles
+import './styles/global.css'
+import './styles/pages/landing.scss'
+
+//SECTION Images
+import logoImg from './images/logo.svg'
+
+function App() {
+    return (
+        <div id="page-landing">
+            <div className="content-wrapper">
+                <img src={logoImg} alt="Happy" />
+            
+            <main>
+                <h1>Leve felicidade para o mundo</h1>
+                <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+            </main>
+
+            <div className="location">
+                <strong>São Paulo</strong>
+                <span>São Paulo</span>
+            </div>
+
+            <a href="/" className="enter-app">
+              >
+            </a>
+          </div>
+        </div>
+    )
+}
+
+export default App
