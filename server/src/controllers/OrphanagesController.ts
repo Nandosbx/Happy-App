a1e8f1ba2f5b720dfe3a40b440ca49f737b29c7e
Fernando Batista <nandosbx@gmail.com>
2020-10-14 17:08:57 -0300

    ErrorHandler, Cors

--- server/src/controllers/OrphanagesController.ts
+++ server/src/controllers/OrphanagesController.ts
@@ -2,6 +2,8 @@ import {Request, Response} from  'express'
 import { getRepository } from 'typeorm'
 import orphanageView from '../views/orphanages_view'
 import Orphanage from '../models/Orphanage'
+import * as Yup from 'yup'
+import { array } from 'yup'
 
 export default {
 
@@ -13,7 +15,7 @@ export default {
       relations: ['images']
     })
 
-    return response.status(200).json(orphanages)
+    return response.status(200).json(orphanageView.renderMany(orphanages))
   },
 
   // ANCHOR Show
@@ -51,7 +53,7 @@ export default {
       return { path: image.filename }
     })
 
-    const orphanage = orphanagesRepository.create({
+    const data = {
       name,
       latitude,
       longitude,
@@ -60,7 +62,26 @@ export default {
       opening_hours,
       open_on_weekends,
       images
+    }
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      openings: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: array(Yup.object().shape({
+        path: Yup.string().required()
+      }))
     })
+
+    await schema.validate(data, {
+      abortEarly: false,
+    })
+
+    const orphanage = orphanagesRepository.create(data)
 
     await orphanagesRepository.save(orphanage)
 
