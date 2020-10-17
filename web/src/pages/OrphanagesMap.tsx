f275a228ff67655c693ddca2775f8d45ff700469
Fernando Batista <nandosbx@gmail.com>
2020-10-16 21:09:41 -0300

    Create Orphanage

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -56,14 +56,14 @@ function OrphanagesMap() {
                         className="map-popup"
                     >
                         Casa das Crianças
-                        <Link to="/">
+                        <Link to="/orphanages/1">
                             <FiArrowRight size={20} color="#FFF" />
                         </Link>
                     </Popup>
                 </Marker>
             </Map>
 
-            <Link to="" className="create-orphanage">
+            <Link to="/orphanages/create" className="create-orphanage">
                 <FiPlus size={32} color="#FFF" />
             </Link>
         </div>
